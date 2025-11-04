adac27950aa32af52b997ca3135119424fd6853e
kev1nandreas <kevin.andreascn@gmail.com>
2025-11-04 13:06:47 +0700

    style(404): adjust font and style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,24 @@
-import Image from "next/image";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+	subsets: ["latin"],
+	weight: ["400", "700", "800"],
+});
 
 export default function NotFound() {
 	return (
 		<div className="h-screen w-full bg-[url('/not-found.png')] bg-center bg-cover relative">
 			<div className="absolute flex w-full h-full bg-black/80 z-10 items-center justify-center">
-				<div className="max-w-150 flex flex-col items-center text-center gap-3 p-4">
-					<h1 className="text-white text-8xl md:text-9xl font-bold mb-5">
+				<div
+					className={`max-w-[810px] flex flex-col items-center text-center gap-3 p-4 ${montserrat.className}`}
+				>
+					<h1 className="text-white text-8xl md:text-[214px] font-extrabold mb-5">
 						404
 					</h1>
-					<span className="text-white text-3xl md:text-4xl font-bold">
+					<span className="text-white text-3xl md:text-[60px] font-extrabold">
 						Something went wrong
 					</span>
-					<span className="text-white text-xl md:text-2xl">
+					<span className="text-white text-xl md:text-[36px] font-medium">
 						Please wait a moment, or you can reach out to the admin if needed.
 					</span>
 				</div>
