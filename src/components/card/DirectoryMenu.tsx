424d9090b0e5d1ef5a349500ea4a68a340556082
Kevin Andreas <138026523+kev1nandreas@users.noreply.github.com>
2025-11-05 23:32:35 +0700

    feat: admin document page (#11)
    * feat: admin document page
    
    * fix: adjust minor styling
    
    ---------
    
    Co-authored-by: Justin Chow <justin.chow05@gmail.com>

---
+++ src/components/card/DirectoryMenu.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+interface DirectoryMenuProps {
+	title: string;
+	description: string;
+	linkHref: string;
+}
+
+export default function DirectoryMenu({
+	title,
+	description,
+	linkHref,
+}: DirectoryMenuProps) {
+	return (
+		<div className="px-6 py-4 border rounded-lg bg-blue-600 text-white flex gap-14 flex-col">
+			<div>
+				<h1 className="font-bold text-[64px]">{title}</h1>
+				<p className="font-semibold text-[16px]">{description}</p>
+			</div>
+			<Link
+				href={linkHref}
+				className="w-full bg-white rounded-lg text-sm text-blue-600 px-4 py-2 text-center font-semibold hover:bg-gray-100 transition-all duration-200 ease-in-out"
+			>
+				Check Package
+			</Link>
+		</div>
+	);
+}
