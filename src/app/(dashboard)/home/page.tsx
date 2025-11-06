482f560743c16b389371fc72cf0c65bc044609e9
vinsa61 <vinsarioshen610@gmail.com>
2025-11-06 23:20:18 +0700

    feat: add cards in home page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,31 +1,67 @@
 import { MoveUpRight } from "lucide-react";
+import DirectoryMenu from "@/components/card/DirectoryMenu";
 import SummaryCard from "@/components/card/SummaryCard";
 import RecapChart from "@/components/chart/RecapChart";
 
 export default function HomePage() {
 	return (
-		<div className="flex flex-col lg:flex-row w-full py-4 px-4 md:px-8">
-			<div className="flex flex-col gap-2 items-center sm:items-start">
-				<SummaryCard
-					title="Total Documents"
-					value="531"
-					variant="primary"
-					icon={MoveUpRight}
-				/>
-				<SummaryCard
-					title="Total Comments"
-					value="832"
-					variant="white"
-					icon={MoveUpRight}
-				/>
-				<SummaryCard
-					title="Total Reviewed Docs"
-					value="256"
-					variant="white"
-					icon={MoveUpRight}
-				/>
+		<div className="flex flex-col w-full py-4 px-4 md:px-8">
+			<div className="flex flex-col lg:flex-row">
+				<div className="flex flex-col gap-2 items-center sm:items-start">
+					<SummaryCard
+						title="Total Documents"
+						value="531"
+						variant="primary"
+						icon={MoveUpRight}
+					/>
+					<SummaryCard
+						title="Total Comments"
+						value="832"
+						variant="white"
+						icon={MoveUpRight}
+					/>
+					<SummaryCard
+						title="Total Reviewed Docs"
+						value="256"
+						variant="white"
+						icon={MoveUpRight}
+					/>
+				</div>
+				<RecapChart />
 			</div>
-			<RecapChart />
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-8 px-16 max-xl:px-6 max-xl:py-8 max-md:p-4">
+				{directoryData.map((item) => (
+					<DirectoryMenu
+						key={item.title}
+						title={item.title}
+						description={item.description}
+						linkHref={item.linkHref}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
+
+const directoryData = [
+	{
+		title: "FPSO ITS",
+		description: "Floating Production, Storage, and Offloading",
+		linkHref: "/admin/document",
+	},
+	{
+		title: "OLNG ITS",
+		description: "Onshore LNG (Liquefied Natural Gas)",
+		linkHref: "/admin/document",
+	},
+	{
+		title: "FPSO ITB",
+		description: "Floating Production, Storage, and Offloading",
+		linkHref: "/admin/document",
+	},
+	{
+		title: "OLNG ITB",
+		description: "Onshore LNG (Liquefied Natural Gas)",
+		linkHref: "/admin/document",
+	},
+];
