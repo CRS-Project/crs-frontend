d9f5189e7fc3bc31c9b50d072e5c45e84ce3f682
Justin Chow <justin.chow05@gmail.com>
2025-11-07 01:16:28 +0700

    fix: adjust styling

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
 				</div>
 				<RecapChart />
 			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-8 px-16 max-xl:px-6 max-xl:py-8 max-md:p-4">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full pt-8">
 				{directoryData.map((item) => (
 					<DirectoryMenu
 						key={item.title}
@@ -47,21 +47,21 @@ const directoryData = [
 	{
 		title: "FPSO ITS",
 		description: "Floating Production, Storage, and Offloading",
-		linkHref: "/admin/document",
+		linkHref: "/admin/documents/fpso-its",
 	},
 	{
 		title: "OLNG ITS",
 		description: "Onshore LNG (Liquefied Natural Gas)",
-		linkHref: "/admin/document",
+		linkHref: "/admin/documents/olng-its",
 	},
 	{
 		title: "FPSO ITB",
 		description: "Floating Production, Storage, and Offloading",
-		linkHref: "/admin/document",
+		linkHref: "/admin/documents/fpso-itb",
 	},
 	{
 		title: "OLNG ITB",
 		description: "Onshore LNG (Liquefied Natural Gas)",
-		linkHref: "/admin/document",
+		linkHref: "/admin/documents/olng-itb",
 	},
 ];
