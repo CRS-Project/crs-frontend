d83e5433e389057c92113fb7e1cae54a2d5f5812
vinsa61 <vinsarioshen610@gmail.com>
2025-11-10 21:01:31 +0700

    feat: add dashboard data page

--- src/components/card/SummaryCard.tsx
+++ src/components/card/SummaryCard.tsx
@@ -5,32 +5,32 @@ import clsxm from "@/lib/clsxm";
 const CardVariant = ["primary", "white"] as const;
 
 interface SummaryCardProps {
-	title: string;
-	value: string;
-	variant: (typeof CardVariant)[number];
-	icon?: LucideIcon;
+  title: string;
+  value: string;
+  variant: (typeof CardVariant)[number];
+  icon?: LucideIcon;
 }
 
 export default function SummaryCard({
-	title,
-	value,
-	variant = "primary",
-	icon: Icon,
+  title,
+  value,
+  variant = "primary",
+  icon: Icon,
 }: SummaryCardProps) {
-	return (
-		<Card
-			className={clsxm("w-[280px] px-6 py-4 rounded-lg", [
-				variant === "primary" && ["bg-blue-500 text-white"],
-				variant === "white" && ["bg-white text-primary-1000"],
-			])}
-		>
-			<CardHeader className="flex justify-between p-1">
-				<span className="text-base font-semibold">{title}</span>
-				{Icon && <Icon size={20} />}
-			</CardHeader>
-			<CardBody className="text-2xl sm:text-4xl lg:text-5xl font-semibold overflow-visible p-1">
-				{value}
-			</CardBody>
-		</Card>
-	);
+  return (
+    <Card
+      className={clsxm("min-w-[280px] w-full px-6 py-4 rounded-lg lg:gap-2", [
+        variant === "primary" && ["bg-blue-500 text-white"],
+        variant === "white" && ["bg-white text-blue-500"],
+      ])}
+    >
+      <CardHeader className="flex justify-between p-1">
+        <span className="text-base font-semibold">{title}</span>
+        {Icon && <Icon size={20} />}
+      </CardHeader>
+      <CardBody className="text-2xl sm:text-4xl lg:text-5xl font-semibold overflow-visible p-1">
+        {value}
+      </CardBody>
+    </Card>
+  );
 }
