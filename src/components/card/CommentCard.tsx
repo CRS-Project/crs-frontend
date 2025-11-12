cb15d31ed9f0f8abe2f736b94a232c781c838b91
kev1nandreas <kevin.andreascn@gmail.com>
2025-11-12 07:30:50 +0700

    fix(comment): authorization issue in admin role

--- src/components/card/CommentCard.tsx
+++ src/components/card/CommentCard.tsx
@@ -163,17 +163,17 @@ export default function CommentCard({
 								</button>
 								{comments?.status === null && (
 									<>
-										{user?.role === ROLE.SUPERADMIN ||
-											(comments.user_comment.name === user?.name && (
-												<button
-													type="button"
-													onClick={handleEditClick}
-													className="w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-50 flex items-center gap-2 cursor-pointer"
-												>
-													<Pencil className="w-[12px] h-[12px]" />
-													Edit
-												</button>
-											))}
+										{(user?.role === ROLE.SUPERADMIN ||
+											comments.user_comment.name === user?.name) && (
+											<button
+												type="button"
+												onClick={handleEditClick}
+												className="w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-50 flex items-center gap-2 cursor-pointer"
+											>
+												<Pencil className="w-[12px] h-[12px]" />
+												Edit
+											</button>
+										)}
 										<button
 											type="button"
 											onClick={handleReplyClick}
@@ -184,17 +184,17 @@ export default function CommentCard({
 										</button>
 									</>
 								)}
-								{user?.role === ROLE.SUPERADMIN ||
-									(comments.user_comment.name === user?.name && (
-										<button
-											type="button"
-											onClick={handleDeleteClick}
-											className="w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-red-50 flex items-center gap-2 cursor-pointer"
-										>
-											<Trash className="w-[12px] h-[12px]" />
-											Delete
-										</button>
-									))}
+								{(user?.role === ROLE.SUPERADMIN ||
+									comments.user_comment.name === user?.name) && (
+									<button
+										type="button"
+										onClick={handleDeleteClick}
+										className="w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-red-50 flex items-center gap-2 cursor-pointer"
+									>
+										<Trash className="w-[12px] h-[12px]" />
+										Delete
+									</button>
+								)}
 							</div>
 						)}
 					</div>
