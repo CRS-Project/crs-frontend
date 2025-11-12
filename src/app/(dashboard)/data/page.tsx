c900abb1775278a218714a92947b49e8fb5132a7
Justin Chow <justin.chow05@gmail.com>
2025-11-12 16:06:07 +0700

    fix: fix data dasboard not appear

--- src/app/(dashboard)/data/page.tsx
+++ src/app/(dashboard)/data/page.tsx
@@ -47,74 +47,72 @@ export default function DataPage() {
 					))}
 				</div>
 			)}
-			{user?.role !== "SUPER ADMIN" ||
-				(selectedPackage !== null && (
-					<>
-						<div className="w-full relative h-[210px] rounded-xl overflow-hidden">
-							<Image
-								src="/images/dashboard/profile/bg-header.png"
-								alt="header"
-								width={3000}
-								height={1000}
-								className="w-full h-full object-cover -z-10"
-							/>
-							<div className="absolute top-4 left-8">
-								<div className="text-white space-y-2">
-									<h1 className="font-bold text-[64px]">
-										{user?.role !== "SUPER ADMIN"
-											? packages?.name
-											: selectedPackage?.name}
-									</h1>
-									<p className="font-semibold text-[16px] -mt-4 mb-4">
-										{user?.role !== "SUPER ADMIN"
-											? packages?.description
-											: selectedPackage?.description}
-									</p>
-									{user?.role === "SUPER ADMIN" && (
-										<button
-											type="button"
-											onClick={() => setSelectedPackage(null)}
-											className="w-full bg-white cursor-pointer rounded-lg text-sm text-blue-600 px-4 py-2 text-center font-semibold hover:bg-gray-100 transition-all duration-200 ease-in-out"
-										>
-											Back to Package List
-										</button>
-									)}
-								</div>
+			{(user?.role !== "SUPER ADMIN" ||
+				(user?.role === "SUPER ADMIN" && selectedPackage !== null)) && (
+				<>
+					<div className="w-full relative h-[210px] rounded-xl overflow-hidden">
+						<Image
+							src="/images/dashboard/profile/bg-header.png"
+							alt="header"
+							width={3000}
+							height={1000}
+							className="w-full h-full object-cover -z-10"
+						/>
+						<div className="absolute top-4 left-8">
+							<div className="text-white space-y-2">
+								<h1 className="font-bold text-[64px]">
+									{user?.role !== "SUPER ADMIN"
+										? packages?.name
+										: selectedPackage?.name}
+								</h1>
+								<p className="font-semibold text-[16px] -mt-4 mb-4">
+									{user?.role !== "SUPER ADMIN"
+										? packages?.description
+										: selectedPackage?.description}
+								</p>
+								{user?.role === "SUPER ADMIN" && (
+									<button
+										type="button"
+										onClick={() => setSelectedPackage(null)}
+										className="w-full bg-white cursor-pointer rounded-lg text-sm text-blue-600 px-4 py-2 text-center font-semibold hover:bg-gray-100 transition-all duration-200 ease-in-out"
+									>
+										Back to Package List
+									</button>
+								)}
 							</div>
 						</div>
-						<h1 className="font-bold text-3xl mb-2 mt-8">
-							Statistics Documents & Comments
-						</h1>
-						<RecapChart packageId={packageId} />
-						<div className="my-8 grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
-							<SummaryCard
-								title="Total Area Of Concern"
-								value={cardData?.data?.total_area_of_concern || 0}
-								variant="primary"
-							/>
-							<SummaryCard
-								title="Total Documents"
-								value={cardData?.data?.total_documents || 0}
-								variant="white"
-							/>
-							<SummaryCard
-								title="Total Comments"
-								value={cardData?.data?.total_comments || 0}
-								variant="white"
-							/>
-							<SummaryCard
-								title="Total Comment Rejected"
-								value={cardData?.data?.total_comment_rejected || 0}
-								variant="white"
-							/>
-						</div>
-						<h1 className="font-bold text-3xl mb-2">
-							Statistics Comment Users
-						</h1>
-						<ReviewRecapChart packageId={packageId} />
-						<CommentUserTable packageId={packageId} />
-					</>
-				))}
+					</div>
+					<h1 className="font-bold text-3xl mb-2 mt-8">
+						Statistics Documents & Comments
+					</h1>
+					<RecapChart packageId={packageId} />
+					<div className="my-8 grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
+						<SummaryCard
+							title="Total Area Of Concern"
+							value={cardData?.data?.total_area_of_concern || 0}
+							variant="primary"
+						/>
+						<SummaryCard
+							title="Total Documents"
+							value={cardData?.data?.total_documents || 0}
+							variant="white"
+						/>
+						<SummaryCard
+							title="Total Comments"
+							value={cardData?.data?.total_comments || 0}
+							variant="white"
+						/>
+						<SummaryCard
+							title="Total Comment Rejected"
+							value={cardData?.data?.total_comment_rejected || 0}
+							variant="white"
+						/>
+					</div>
+					<h1 className="font-bold text-3xl mb-2">Statistics Comment Users</h1>
+					<ReviewRecapChart packageId={packageId} />
+					<CommentUserTable packageId={packageId} />
+				</>
+			)}
 		</div>
 	);
 }
