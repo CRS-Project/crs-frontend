45bd22234282d90fe1fccf5f49ec7b0dbcb20d3c
Kevin Andreas <138026523+kev1nandreas@users.noreply.github.com>
2025-11-16 15:13:38 +0700

    fix(comment): fix filtering issue in document selection (#77)

--- src/app/(dashboard)/concern/[id]/[id_concern]/[id_document]/_hooks/useGetDocument.ts
+++ src/app/(dashboard)/concern/[id]/[id_concern]/[id_document]/_hooks/useGetDocument.ts
@@ -2,10 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { get } from "@/service/call";
 import { MAIN_ENDPOINT } from "@/service/endpoint";
 
-export const useGetDocument = () => {
+export const useGetDocument = (packageId: string) => {
+	const params = {
+		filter: packageId,
+		filter_by: "package_id",
+	};
+
 	return useQuery({
 		queryFn: async () => {
-			const { Kind, OK } = await get(MAIN_ENDPOINT.Document.FetchDocuments);
+			const { Kind, OK } = await get(
+				MAIN_ENDPOINT.Document.FetchDocuments,
+				params,
+			);
 			if (!OK) {
 				const error = Kind as any;
 				throw new Error(error.message || error.Message);
