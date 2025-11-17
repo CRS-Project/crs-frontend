4f291247e43c5546f6ded6e2bf47f87b89468b7f
Justin Chow <justin.chow05@gmail.com>
2025-11-18 01:12:59 +0700

    fix: remove href from data button

--- src/app/(dashboard)/data/page.tsx
+++ src/app/(dashboard)/data/page.tsx
@@ -29,13 +29,26 @@ export default function DataPage() {
 			{user?.role === "SUPER ADMIN" && selectedPackage === null && (
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:mb-8">
 					{data?.data?.map((item: Package) => (
-						<DirectoryMenu
+						<div
+							className="px-6 py-5 border rounded-lg bg-blue-600 text-white flex gap-14 flex-col justify-between"
 							key={item.id}
-							title={item.name}
-							description={item.description}
-							linkHref={`/documents/${item.id}`}
-							value="Check Documents"
-						/>
+						>
+							<div>
+								<h1 className="font-bold text-[64px] leading-[1]">
+									{item.name}
+								</h1>
+								<p className="font-semibold text-[16px] mt-2">
+									{item.description}
+								</p>
+							</div>
+							<button
+								type="button"
+								onClick={() => setSelectedPackage(item)}
+								className="w-full bg-white cursor-pointer rounded-lg text-sm text-blue-600 px-4 py-2 text-center font-semibold hover:bg-gray-100 transition-all duration-200 ease-in-out"
+							>
+								Check Data
+							</button>
+						</div>
 					))}
 				</div>
 			)}
